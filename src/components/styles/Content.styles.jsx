11bfd0d29e1b1f82fd7c1e9c72e3abaa11f96fab
Jazaltron <jazaltron10@gmail.com>
2021-10-07 20:13:54 +0100

    Switched to using styled-components

---
+++ src/components/styles/Content.styles.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const StyledContent = styled.div`
+    padding: 40px 0;
+    transition: ease-in .4s;
+    text-align: center;
+    background: black;
+    color: white;
+    h1{
+        color: yellow;
+    }
+
+    &:hover{
+        background-color: black;
+    }
+`
+
