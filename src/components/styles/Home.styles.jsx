f0ee5b266c6210a396d0f834647d7d1f641990b0
Jazaltron <jazaltron10@gmail.com>
2021-12-20 08:50:32 +0100

    A few modifications to the existing components

--- src/components/styles/Home.styles.jsx
+++ src/components/styles/Home.styles.jsx
@@ -1,5 +1,24 @@
 import styled from "styled-components";
 
 export const HomeStyles = styled.div`
-
+h2{
+    color: deepskyblue;
+    margin-bottom: .5rem;
+    /* display: flex; */
+}
+button{
+    width: 6rem;
+    height: 3rem;
+    padding: 1rem;
+    outline: none;
+    border: none;
+    border-radius: 0.5rem;
+    background-color: deepskyblue;
+    
+    &:hover{
+        background-color:black;
+        color: white;
+        cursor: pointer;
+    }
+}
 `
--- src/components/styles/Navbar.styles.jsx
+++ src/components/styles/Navbar.styles.jsx
@@ -10,22 +10,33 @@ export const NavbarStyles = styled.nav`
     margin: 0 auto;
     border-bottom: 1.5px solid white;
     text-align: center;
-`
+
+    h1{
+        font-family: cursive;
+    }
+    @media(max-width: ${({theme})=> theme.IpadMobile}){
+        padding: 1rem;
+        display: block;
+    }
+    `
 
 export const LinkStyles = styled.div`
         margin-left: auto;
-    a{
-        margin-left: 16px;
-        text-decoration: none;
-        color: white;
-        padding: 6px;
-        background-color: ${({theme}) => theme.colors.PrimoCols};
-        border-radius: 10px;
-    }
-    a:hover{
-        /* color: #f1356d; */
-        /* color:  hsl(192, 100%, 9%); */
-        color: white;
-        transform: scale(1.5);
-    }
-`
+        a {
+            margin-left: 16px;
+            text-decoration: none;
+            color: white;
+            padding: 6px;
+            background-color: ${({theme}) => theme.colors.TransCols};
+            border-radius: 10px;
+        }
+        a:hover{
+            /* color: #f1356d; */
+            /* color:  hsl(192, 100%, 9%); */
+            color: white;
+            transform: scale(1.5);
+        }
+        @media(max-width: ${({theme})=> theme.IpadMobile}){
+            padding: 1rem;
+        }
+        `
