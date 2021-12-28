9427ac8445ffeff306715882dd8afe7d5a6d5384
Jazaltron <jazaltron10@gmail.com>
2021-12-28 21:14:42 +0100

    WIP

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,10 +4,16 @@ const Home = () => {
     const handleClick = () => {
         console.log("Hello, Ninjas");
     }
+    
+    const handleClickAgain = ()=>{
+        console.log("Hello, Ninjas");
+    }
+    
     return (
         <HomeStyles>
             <h2>Homepage</h2>
             <button onClick={handleClick}>Click Me</button>
+            <button>Click Me Again</button>
         </HomeStyles>
     )
 }
