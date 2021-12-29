0f5c27c9929ed8a3dfba19f7a3b7e33de9c99cb5
Jazaltron <jazaltron10@gmail.com>
2021-12-29 19:55:08 +0100

    Click Events, Done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,27 @@
-import { HomeStyles } from "./styles/Home.styles"
+import { HomeStyles, BtnStyles } from "./styles/Home.styles"
 const Home = () => {
 
-    const handleClick = () => {
-        console.log("Hello, Ninjas");
+    const handleClick = (e) => {
+        console.log("Hello, Ninjas", e);
     }
     
-    const handleClickAgain = ()=>{
-        console.log("Hello, Ninjas");
+    const handleClickAgain = (name, e) =>{
+        console.log(`Hello ${name}`, e.target);
     }
     
     return (
         <HomeStyles>
             <h2>Homepage</h2>
+            <BtnStyles>
             <button onClick={handleClick}>Click Me</button>
-            <button>Click Me Again</button>
+            <button onClick={(e)=>{handleClickAgain('Mario', e)}}>Click Me Again</button>
+            </BtnStyles>
         </HomeStyles>
     )
 }
+// Using Anonymous functions, ()=>{}which are functions that are not held in memory (Constants or variables) to handle click events 
+// We wrap functions that take in parameters when a click event is fired in anonymous functions in order to prevent it from automatically setting of irrespective of the event.
+// it is the anon function that gets access to the event object.
 
 
 export default Home
--- src/components/styles/Home.styles.jsx
+++ src/components/styles/Home.styles.jsx
@@ -7,10 +7,19 @@ align-items: center;
 h2{
     color: ${({theme}) => theme.colors.limecols};;
     margin-bottom: .5rem;
-    /* display: flex; */
 }
+`
+export const BtnStyles = styled.div`
+background-color: ${({theme})=> theme.colors.TransCols};
+height: 5rem;
+border-radius: 2rem;
+display: flex;
+justify-content: center;
+align-items: center;
+padding: .2rem 1rem;
+
 button{
-    margin-bottom: .5rem;
+    margin: .5rem;
     width: 5rem;
     height: 2.5rem;
     padding: .25rem;
@@ -18,6 +27,7 @@ button{
     border: none;
     border-radius: 0.5rem;
     background-color: ${({theme})=> theme.colors.limecols};
+    /* margin-bottom: .5rem; */
     
     &:hover{
         background-color:black;
